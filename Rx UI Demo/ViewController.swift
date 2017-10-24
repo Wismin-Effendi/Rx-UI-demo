e370566246cb097fb4c63e720831bbe59b4462ac
Wismin-Effendi <29092240+Wismin-Effendi@users.noreply.github.com>
2017-10-24 12:14:32 -0500

    Ready for demo.

--- Rx UI Demo/ViewController.swift
+++ Rx UI Demo/ViewController.swift
@@ -7,12 +7,66 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tapGestureRecognizer: UITapGestureRecognizer!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var segmentedLabel: UILabel!
+    @IBOutlet weak var textViewCopyLabel: UILabel!
+    @IBOutlet weak var textViewCountLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var stepperTextLabel: UITextField!
+    @IBOutlet weak var stepper: UIStepper!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setupTapGestureRecognizer()
+        setupStepper()
+        setupSegmentedControl()
+        setupTextView()
+    }
+    
+    private func setupTapGestureRecognizer() {
+        tapGestureRecognizer.rx.event
+            .bind { [unowned self] _ in
+                self.view.endEditing(true)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func setupStepper() {
+        stepper.rx.value
+            .map { String(Int($0)) }
+            .bind(to: stepperTextLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+    
+    private func setupSegmentedControl() {
+        segmentedControl.rx.value
+            .skip(0)
+            .bind { [unowned self] in
+                self.segmentedLabel.text = "Selected segment: \($0)"
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func setupTextView() {
+        textView.rx.text.orEmpty.asDriver()
+            .drive(textViewCopyLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        textView.rx.text.orEmpty.asDriver()
+            .map {
+                "Character count: \($0.characters.count)"
+            }
+            .drive(textViewCountLabel.rx.text)
+            .disposed(by: disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
