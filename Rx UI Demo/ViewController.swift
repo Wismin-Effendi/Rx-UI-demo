12b2cf1f48b31cd1c4b6b7dcc5feb61a2e7fb86b
Wismin-Effendi <29092240+Wismin-Effendi@users.noreply.github.com>
2017-10-24 11:19:05 -0500

    Initial Commit

---
+++ Rx UI Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rx UI Demo
+//
+//  Created by Wismin Effendi on 10/24/17.
+//  Copyright © 2017 Wismin Effendi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
